4cacf103b61f1351b129110dc852022a4470a13b
sebastianmichko <sebastianmichko@gmail.com>
2019-09-18 04:04:42 +0000

    Entire assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,49 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.sort.last(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  combos = arr.uniq.combination(2)
+  !!combos.detect { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+ !!/^[^aeiou\W]/i.match(s) 
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s == "0" 
+        return true
+    end
+   !!/^[01]*(00)$/.match(s) 
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    attr_accessor :isbn, :price
+    
+    def initialize(isbn, price)
+        raise ArgumentError if isbn.empty?
+        raise ArgumentError if price <=0
+
+        @isbn = isbn
+        @price = price
+    end
+    
+    def price_as_string()
+        format("$%.2f", @price)
+    end
 end
